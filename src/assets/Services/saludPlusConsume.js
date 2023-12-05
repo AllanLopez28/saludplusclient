fb3ccd290b0733ef24f1b19ec6f7716d76df7545
AllanLopez28 <104786630+AllanLopez28@users.noreply.github.com>
2023-12-04 20:06:15 -0600

    api

--- src/assets/Services/saludPlusConsume.js
+++ src/assets/Services/saludPlusConsume.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = '${import.meta.env.VITE_API_URL}/api';
+const API_URL = 'saludplusweb-production.up.railway.app/api';
 
 export const login = async (formData) => {
 try {
