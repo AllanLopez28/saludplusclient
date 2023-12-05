0b3ec1cee95fb79fb5f35a440f14cb0a5ae24631
Tu Nombre <you@example.com>
2023-12-04 19:52:53 -0600

    modified api url

--- src/assets/Services/saludPlusConsume.js
+++ src/assets/Services/saludPlusConsume.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'saludplusweb-production.up.railway.app';
+const API_URL = '${import.meta.env.VITE_API_URL}/api';
 
 export const login = async (formData) => {
 try {
